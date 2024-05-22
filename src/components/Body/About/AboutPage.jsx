1d51ecd718778ead119630be40ab6a5bd709a70d
bibek-gotame <bibekgotame011@gmail.com>
2024-05-22 19:28:29 +0545

    Built Mobile_Menu

--- src/components/Body/About/AboutPage.jsx
+++ src/components/Body/About/AboutPage.jsx
@@ -6,38 +6,45 @@ import promise from "../../../assets/promise.png";
 function AboutPage() {
   return (
     <div className="pt-32  px-4 sm:px-20 bg-orange-50">
-      <div className="flex  py-5">
+      <div className="flex flex-col lg:flex-row gap-3  py-5">
         <div className=" lg:w-1/2">
           <p className="font-bold text-4xl text-orange-600">
             About Smart Vision Institute & Consultancy
           </p>
           <p className="text-gray-600 font-semibold text-xl pt-5  ">
-            Welcome to <span className=" text-orange-600">Smart Vision Institute & Consultancy</span>, where learning
-            meets laughter and success is the name of the game. Located in
-            Gramthan-7, Netachowk, Morang, we are not just another educational
-            institute; we are your partner in personal and professional growth.
+            Welcome to{" "}
+            <span className=" text-orange-600">
+              Smart Vision Institute & Consultancy
+            </span>
+            , where learning meets laughter and success is the name of the game.
+            Located in Gramthan-7, Netachowk, Morang, we are not just another
+            educational institute; we are your partner in personal and
+            professional growth.
           </p>
         </div>
         <div className="relative lg:w-1/2">
           <img src={about} alt="about" />
-          <img src={aboutUs} alt="about" className="absolute top-0 right-0 w-[10rem]" />
+          <img
+            src={aboutUs}
+            alt="about"
+            className="absolute top-0 right-0 w-[10rem]"
+          />
         </div>
       </div>
-      <div className="flex py-10">
-      <div className="img lg:w-1/2">
+      <div className="flex flex-col lg:flex-row gap-3 py-10">
+        <div className="img lg:w-1/2">
           <img src={mission} alt="mission" />
         </div>
-        <div className="content lg:w-1/2">
+        <div className=" lg:w-1/2">
           <p className="font-bold text-4xl text-orange-600">Our Mission</p>
           <p className="text-gray-600 font-semibold text-xl pt-5 ">
-            At <span className=" text-orange-600">Smart Vision</span>, we are dedicated to providing high-quality
-            education and training across various fields. From language courses
-            to computer training and professional development programs, we equip
-            our students with the skills and knowledge they need to conquer the
-            world.
+            At <span className=" text-orange-600">Smart Vision</span>, we are
+            dedicated to providing high-quality education and training across
+            various fields. From language courses to computer training and
+            professional development programs, we equip our students with the
+            skills and knowledge they need to conquer the world.
           </p>
         </div>
-       
       </div>
       {/* <div>
         <div className="content">
@@ -48,12 +55,19 @@ function AboutPage() {
           <img src="" alt="" />
         </div>
       </div> */}
-      <div className="flex  py-5">
+      <div className="flex flex-col lg:flex-row gap-3  py-5">
         <div className=" lg:w-1/2">
           <p className="font-bold text-4xl text-orange-600">Our Promise</p>
-          <p className="text-gray-600 font-semibold text-xl pt-5 ">At <span className=" text-orange-600">Smart Vision Institute & Consultancy</span>, we don't just teach; we inspire, we challenge, and we transform. Join us and discover a place where education isn't just about textbooks and lectures—it's about creating a future you'll be proud of.
-
-</p>
+          <p className="text-gray-600 font-semibold text-xl pt-5 ">
+            At{" "}
+            <span className=" text-orange-600">
+              Smart Vision Institute & Consultancy
+            </span>
+            , we don't just teach; we inspire, we challenge, and we transform.
+            Join us and discover a place where education isn't just about
+            textbooks and lectures—it's about creating a future you'll be proud
+            of.
+          </p>
         </div>
         <div className="lg:w-1/2">
           <img src={promise} alt="promise" />
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,10 @@
 import { Link } from "react-router-dom";
 import { MENU_ICON_URL } from "../../utils/constant";
 import logo from "../../assets/logo.png";
+import { useState } from "react";
+import { MdCancel } from "react-icons/md";
 function Header() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
   return (
     <div className=" fixed z-20 w-full">
       <div className="flex justify-between items-center w-[95%] my-2 mx-auto  px-4 lg:px-14 py-1  rounded-lg text-[#012F5B] bg-white bg-opacity-30 backdrop-filter backdrop-blur-[10px] shadow-lg  ">
@@ -17,28 +20,16 @@ function Header() {
           </Link>
         </div>
         <div className=" hidden md:flex items-center gap-6 text-lg font-bold">
-          <Link
-            to="/"
-            className="navEffect"
-          >
+          <Link to="/" className="navEffect">
             Home
           </Link>
-          <Link
-            to="about"
-            className="navEffect"
-          >
+          <Link to="about" className="navEffect">
             About
           </Link>
-          <Link
-            to="Services"
-            className="navEffect"
-          >
+          <Link to="Services" className="navEffect">
             Services
           </Link>
-          <Link
-            to="contact"
-            className="navEffect"
-          >
+          <Link to="contact" className="navEffect">
             Testimonials
           </Link>
           <Link
@@ -48,11 +39,45 @@ function Header() {
             Contact Us
           </Link>
         </div>
-        <div className="w-8 md:hidden">
+        <div className="w-8 md:hidden " onClick={() => setIsMenuOpen(true)}>
           <img src={MENU_ICON_URL} alt="menu" />
         </div>
+      </div>
+      {/* mobile_menu */}
+      <div
+        className={` ${
+          isMenuOpen ? "inline-block" : "hidden"
+        } absolute top-0 right-0  w-[90%] h-screen px-5 bg-white  md:hidden`}
+      >
+      
+      
+        <div className="relative flex gap-4 py-10 items-center">
+        <MdCancel className=" absolute right-[-1rem] top-10 text-5xl   " onClick={()=> setIsMenuOpen(false)} />
 
-        {/* mobile_menu */}
+          <img src={logo} alt="logo" className="w-16" />
+          <div className="h-[4rem] w-[3px] bg-orange-600"></div>
+          <div className="text-lg font-bold text-orange-600">
+            Smart Vision <br /> Institute & Consultancy
+          </div>
+        </div>
+        <div className="flex flex-col  gap-4   text-lg font-bold">
+          <Link to="/" className="navEffect" onClick={()=> setIsMenuOpen(false)}>
+            Home
+          </Link>
+          <Link to="about" className="navEffect" onClick={()=> setIsMenuOpen(false)}>
+            About
+          </Link>
+          <Link to="contact" className="navEffect" onClick={()=> setIsMenuOpen(false)}>
+            Testimonials
+          </Link>
+          <Link to="Services" className="navEffect" onClick={()=> setIsMenuOpen(false)}>
+            Services
+          </Link>
+         
+          <Link to="contact" className=" btn w-fit " onClick={()=> setIsMenuOpen(false)}>
+            Contact Us
+          </Link>
+        </div>
       </div>
     </div>
   );
